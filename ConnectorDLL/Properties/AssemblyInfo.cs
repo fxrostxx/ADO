f5c3007541cfba1479ee6674fe7744d267d832a1
fxrostxx <fxrostxx@FXROSTXX-PC>
2026-03-24 19:27:22 +0300

    ConnectorDLL DONE

---
+++ ConnectorDLL/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConnectorDLL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConnectorDLL")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5f5026b8-7042-4f8f-ba80-0db41b3bea94")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ConnectorDLLTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using ConnectorDLL;
+
+namespace ADO
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string connection_string = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Movies_PV_521;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+			Connector connector = new Connector(connection_string);
+
+			Console.WriteLine(connector.GetPrimaryKeyColumnName("Directors"));
+			Console.WriteLine(connector.GetPrimaryKeyColumnName("Movies"));
+
+			connector.Insert
+			(
+				"Directors",
+				"director_id,first_name,last_name",
+				$"{connector.GetNextPrimaryKey("Directors")},John,Singleton"
+			);
+
+			connector.Insert("INSERT Directors (first_name, last_name) VALUES (N'Guy', N'Richie');");
+			Console.WriteLine($"PK Max: {connector.GetMaxPrimaryKey("Directors")}");
+			connector.Select("*", "Directors");
+			Console.WriteLine($"Кол-во записей: {connector.Scalar("SELECT COUNT(*) FROM Directors")}");
+			connector.Select
+			(
+				"title, release_date, first_name, last_name",
+				"Movies, Directors",
+				"director = director_id"
+			);
+			Console.WriteLine($"Кол-во записей: {connector.Scalar("SELECT COUNT(*) FROM Movies")}");
+		}
+	}
+}
